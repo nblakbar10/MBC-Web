918396067abdde8758ae78da5f527347361497f3
Nabil Akbar Pratama <nabilakbarpratama@gmail.com>
2023-04-08 04:53:36 +0800

    fix: bug xendit callback not generating ticket

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -38,6 +38,52 @@ class TicketController extends Controller
         ]);
 
         // $check_payment_status = Transaction::where('external_id','MBC-SmileFest2023-cC6ps')->pluck('payment_status')->first();
+        ////$check_payment_status = Transaction::where('external_id',$external_id)->pluck('payment_status')->first();
+        $mix_ticket = 'ID2023MBC-'.Str::random(11);
+        //barcode
+        $generator = new Picqer\Barcode\BarcodeGeneratorPNG();
+        file_put_contents(public_path('storage/barcode_ticket/').$mix_ticket.'.jpg', $generator->getBarcode('081231723897', $generator::TYPE_CODABAR));
+        // $file_bukti = url($mix_ticket.'.jpg');
+        // dd($file_bukti);
+        // $data_trans = Transaction::where('external_id', 'MBC-SmileFest2023-cC6ps')->get()->first();
+        $data_trans = Transaction::where($external_id, 'external_id')->get();
+        // if ($check_payment_status == 'PAID'){
+        if ($status == "PAID"){
+            Ticket::create([
+                "external_id" => $external_id,
+                "ticket_id" => $mix_ticket,
+                "ticket_name" => $data_trans->name,
+                "email" => $data_trans->email,
+                "phone_number" => $data_trans->phone_number,
+                "payment_method" => $data_trans->payment_method,
+                "ticket_qty" => $data_trans->total_tickets,
+                "ticket_category" => $data_trans->tickets_category,
+                "ticket_status" => "Not redeemed yet",
+                "ticket_barcode" => url($mix_ticket.'.jpg')
+            ]);
+        }
+
+        //sent email :
+        $mailData = [
+            'to' => $data_trans->name,
+            'id_tiket' => $mix_ticket,
+            'nama' => $data_trans->name,
+            'email' => $data_trans->email,
+            'no_hp' => $data_trans->phone_number,
+            'jumlah_tiket' => $data_trans->total_tickets,
+            'jenis_tiket' => $data_trans->tickets_category,
+            'total_pembayaran' => $data_trans->total_amount,
+            'metode_pembayaran' => $data_trans->payment_method,
+            'status_pembayaran' => $status->payment_status, //langsung dari xendit
+            "status_tiket" => "Not redeemed yet",
+            "ticket_barcode" => url($mix_ticket.'.jpg')
+        ];
+        Mail::to($data_trans->email)->send(new SuccessMail($mailData));
+
+    }
+
+    public function generate_ticket(){
+        // $check_payment_status = Transaction::where('external_id','MBC-SmileFest2023-cC6ps')->pluck('payment_status')->first();
         $check_payment_status = Transaction::where('external_id',$external_id)->pluck('payment_status')->first();
         $mix_ticket = 'ID2023MBC-'.Str::random(11);
         //barcode
@@ -78,39 +124,6 @@ class TicketController extends Controller
             "ticket_barcode" => url($mix_ticket.'.jpg')
         ];
         Mail::to($data_trans->email)->send(new SuccessMail($mailData));
-
-        // $check_payment_status = Transaction::where('external_id',$external_id)->pluck('payment_status')->first();
-        // $source_date = Carbon::now();
-        // $parsed_date = Carbon::parse($source_date)->format('d F Y');
-        // $mix_ticket = $parsed_date.Str::random(5);
-        // $data_trans = Transaction::where($external_id, 'external_id')->get();
-
-        // if ($check_payment_status == 'PAID'){
-        //     Ticket::create([
-        //         "external_id" => $external_id,
-        //         "ticket_id" => $mix_ticket,
-        //         "ticket_name" => $data_trans->name,
-        //         "email" => $data_trans->email,
-        //         "phone_number" => $data_trans->phone_number,
-        //         "payment_method" => $data_trans->payment_method,
-        //         "ticket_qty" => $data_trans->total_tickets,
-        //         "ticket_category" => $data_trans->tickets_category,
-        //         "ticket_status" => "Not redeemed"
-        //     ]);
-        // }
-
-        // //sent email :
-        // $mailData = [
-        //     'id_tiket' => $mix_ticket,
-        //     'nama' => $data_trans->name,
-        //     'email' => $data_trans->email,
-        //     'no_hp' => $data_trans->phone_number,
-        //     'jumlah_tiket' => $data_trans->ticket_amount,
-        //     'jenis_tiket' => $data_trans->tickets_category,
-        //     'total_pembayaran' => $data_trans->total_amount,
-        //     'metode_pembayaran' => $data_trans->payment_method,
-        // ];
-        // Mail::to($data_trans->email)->send(new SuccessMail($mailData));
     }
 
 
