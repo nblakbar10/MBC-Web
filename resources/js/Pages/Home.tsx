761c785da65ec390af4f6e44fef0e983b26c864a
RUVNE <dian.galleon@gmail.com>
2023-05-08 21:37:31 +0800

    refactor : change event start-end

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -106,7 +106,7 @@ export default function Home({ promos, discounts }: Props) {
                                 </p>
                                 <p className="flex gap-2 p-2">
                                     <svg className="fill-[#2EA1DA]" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm0 11h6v1h-7v-9h1v8z" /></svg>
-                                    14:00 - 22:00
+                                    16:00 - Selesai
                                 </p>
                             </div>
                             <div className="border-black border-t-2 text-md md:text-xl my- text-[#848383] p-2">
