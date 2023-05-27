d58401a744bee72ac31944938d921b008f8abb02
RUVNE <dian.galleon@gmail.com>
2023-05-27 13:16:02 +0800

    feat WIP : home view

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -2,8 +2,8 @@ import Banner from "@/Components/Jetstream/Banner";
 import { asset } from "@/Models/Helper";
 import { Head } from "@inertiajs/inertia-react";
 import React from "react";
-import PrimaryButton from '@/Components/Jetstream/PrimaryButton';
 import EventIcon from '@mui/icons-material/Event';
+import SearchIcon from "@mui/icons-material/Search";
 
 interface Props {
     children: React.ReactNode;
@@ -19,58 +19,46 @@ export default function AppLayout({ children, title }: Props) {
             </Head>
             <Banner />
             <div className="min-h-screen bg-white flex flex-col">
-                <nav className="flex w-full bg-[#262626] p-5 pl-20">
-                    <div className="flex justify-around m-auto p-3 w-full md:w-5/6">
-                        <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
-                            {/* <!-- logo --> */}
-                            <div className="flex items-center justify-center">
-                                <img
-                                    src={asset('root', 'assets/images/MBC_HD.png')}
-                                    alt="Logo"
-                                    width={200}
-                                />
-                            </div>
-                            {/* <!-- search bar --> */}
-                            <div className="flex items-center justify-between ">
-                                <div className="flex border-2 rounded ">
-                                    <input type="text" className="px-4 py-2 w-80" placeholder="Search..."/>
-                                    <button className="flex items-center justify-center px-4 border-l">
-                                        <svg className="w-6 h-6 text-gray-600" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24">
-                                            <path
-                                                d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-                                        </svg>
-                                    </button>
+                <nav className="bg-gray-800 flex justify-around">
+                    <div className="flex w-full lg:w-5/6 p-5 gap-10 justify-between">
+                        <div className="my-auto">
+                            <img
+                                src={asset('root', 'assets/images/MBC_HD.png')}
+                                alt="Logo"
+                                width={200}
+                            />
+                        </div>
+                        <div className="flex gap-2">
+                            <form className="block text-md my-auto">
+                                <input
+                                    className="p-2 rounded-l-lg w-96"
+                                    placeholder="Cari Event"
+                                >
+                                </input>
+                                <button
+                                    className="py-2 px-3 text-white bg-stone-400 rounded-r-lg hover:bg-stone-600"
+                                >
+                                    <SearchIcon/>
+                                </button>
+                            </form>
+                            <button
+                                className=" px-4 ml-7"
+                            >
+                                <div className="flex-col text-white">
+                                    <EventIcon
+                                        fontSize="large"
+                                    />
+                                    <p className="text-sm ">Events</p>
                                 </div>
-                            </div>
-                            {/* <!-- event icon --> */}
-                            <div className="flex items-center justify-center ">
-                            <button className="flex text-sm text-white border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                                <EventIcon fontSize="large"/>
                             </button>
-                            <div className="h-300 min-h-[2em] w-0.5 bg-[#767171]"></div>
-                            </div>
-
-                            <div className="flex items-center justify-center gap-1">
-                                <PrimaryButton
-                                    type="button"
-                                    className="inline-block rounded bg-[#FFFFFF] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-[#262626]">
-                                    Success
-                                </PrimaryButton>
-                                <PrimaryButton
-                                    type="button"
-                                    className="inline-block rounded-full border-2 border-[#FFFFFF] bg-[#262626] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-[#FFFFFF] ">
-                                    Danger
-                                </PrimaryButton>
-                            </div>
                         </div>
                     </div>
                 </nav>
                 {children}
                 <footer className="w-full h-60 bg-[#767171]">
-                    <div className ="container flex justify-center m-auto p-3 mt-8 ">
+                    <div className="container flex justify-center m-auto p-3 mt-8 ">
                         <div className="object-cover object-center rounded-xl shadow-md h-auto">
-                        <img
+                            <img
                                 src={asset('root', 'assets/images/MBC_HD.png')}
                                 className="justify-center"
                                 width={200}
