c376808431d1c2d57eb54e4b809e68f4eab57cbd
MZIR25 <muhikram1609@gmail.com>
2023-06-05 19:53:58 +0800

    fix : email

--- resources/views/email/Newerticketmail.blade.php
+++ resources/views/email/Newerticketmail.blade.php
@@ -476,7 +476,7 @@
                           <tr>
                             <td class="text-services" style="text-align: left; ">Total Pembelian</td>
                             <td class="text-md">:</td>
-                            <td class="text-services" style="text-align: left; ">{{$mailData['total_pembelian']}}</td>
+                            <td class="text-services" style="text-align: left; "></td>
                           </tr>
                           <tr>
                             <td class="text-services" style="text-align: left; ">Metode Pembayaran</td>
