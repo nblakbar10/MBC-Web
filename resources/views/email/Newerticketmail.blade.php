71b57d6ac69c2b07db66572f28a3941fe61fff43
MZIR25 <muhikram1609@gmail.com>
2023-06-05 20:35:06 +0800

    Fix : email

--- resources/views/email/Newerticketmail.blade.php
+++ resources/views/email/Newerticketmail.blade.php
@@ -403,7 +403,7 @@
     }
     .solid {
         border-style: solid;
-        border-width: 1px;
+        border-width: 5px;
         border-color:rgb(0, 0, 0)}
 
   </style>
@@ -452,7 +452,7 @@
                 <div class="heading-section" style="text-align: center; padding: 0 10px; background-color: #ffffff;">
                     {{-- 767171 --}}
                     <h3>Berikut detail data pemesanan anda yang kami terima:</a></h3>
-                  <table class="solid" role="presentation" border="1" cellpadding="0" cellspacing="0" width="100%" style="padding: 0 10px; margin-right: 0.75rem; border: 5px; ">
+                  <table class="solid" role="presentation" border="1" cellpadding="0" cellspacing="0" width="100%" style=" margin-right: 0.75rem; ">
                     <tr>
                       <td valign="top" style="width: 80% padding-top: 10px; margin-right: 0.75rem; border: solid;">
                         <table role="presentation" cellspacing="0" cellpadding="0" width="100%">
