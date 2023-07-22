8d1e16d2e46e4a836e44ebd3c19c7de3c22c9371
MZIR25 <muhikram1609@gmail.com>
2023-07-22 10:00:33 +0800

    Fix : Dashboard

--- resources/js/Pages/Admin/Dashboard.tsx
+++ resources/js/Pages/Admin/Dashboard.tsx
@@ -12,7 +12,7 @@ export default function Dashboard(props: Props) {
 		<DashboardAdminLayout
 			title="Dashboard"
 		>
-            <div className="max-w-7xl mx-auto sm:px-6 md:px-6 lg:px-5 xl:px-6 py-12 ">
+            <div className="max-w-7xl mx-auto sm:px-6 md:px-6 lg:px-3 xl:px-6 mt-10 ">
 				{/* <div className="bg-white shadow-sm shadow-neutral-700 overflow-hidden sm:rounded-lg p-4"> */}
 					<div className="max-w-7xl">
 						<div className="grid grid-cols-1 lg:grid-cols-4 p-3 gap-10 justify-around">
@@ -75,7 +75,7 @@ export default function Dashboard(props: Props) {
 							</div>
 							<div className="rounded-lg flex-col shadow-sm shadow-neutral-700 flex-1 border-neutral-400 text-white">
 								<div className='basis-  4/5 bg-[#E05E36] px-10 py-5 rounded-t-lg text-center'>
-									<div className="text-lg font-semibold mb-2">Total Pengguna pada Sistem</div>
+									<div className="text-lg font-semibold mb-2">Total Pengguna pada Sistem MBC</div>
 									<div className="text-2xl font-extrabold">
 										<div className="stat-value">1 Pengguna</div>
 
@@ -96,9 +96,9 @@ export default function Dashboard(props: Props) {
 
 						</div>
 
-					{/* </div> */}
+					</div>
 				</div>
-                <div className="max-w-7xl mx-auto sm:px-6 md:px-6 lg:px-6 py-12 ">
+                <div className="max-w-7xl mx-auto sm:px-6 md:px-6 lg:px-6 py-2 ">
                     <div className="bg-white shadow-sm shadow-neutral-700 overflow-hidden sm:rounded-lg p-4">
                         <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
@@ -139,7 +139,15 @@ export default function Dashboard(props: Props) {
                         </div>
                     </div>
                 </div>
-			</div>
+                <div className="max-w-7xl mx-auto sm:px-6 md:px-6 lg:px-6 xl:px-6 py-2 ">
+                    <div className="bg-white shadow-sm shadow-neutral-700 overflow-hidden sm:rounded-lg p-4">
+                    </div>
+                </div>
+                <div className="max-w-7xl mx-auto sm:px-6 md:px-6 lg:px-6 xl:px-6 py-2 ">
+                    <div className="bg-white shadow-sm shadow-neutral-700 overflow-hidden sm:rounded-lg p-4">
+                    </div>
+                </div>
+
 		</DashboardAdminLayout>
 	);
 }
