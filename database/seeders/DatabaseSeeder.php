b65a8662d2bb93fcbdbd54e3a22dbe98ffa35b80
Nabil Akbar Pratama <36075335+nblakbar10@users.noreply.github.com>
2023-07-25 22:37:22 +0800

    turn off seeder transaction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             // RoleSeeder::class,
-            // UserSeeder::class,
-            TransactionSeeder::class,
+            UserSeeder::class,
+            // TransactionSeeder::class,
         ]);
     }
 }
